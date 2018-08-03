efc16d5ba6618efea4186481b576f88d231799ca
Paulina Dabrowska <14799673+Trufs@users.noreply.github.com>
2018-08-03 21:49:12 +0200

    Add attribution for third part api

--- src/Side.js
+++ src/Side.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import Filter from './Filter.js';
 import Locations from './Locations.js';
+import img from './img/poweredby-darkbackground.png'
 
 class Side extends Component {
   render() {
     return (
+
       <nav>
         <Filter />
         <Locations places = {this.props.places}/>
+        <div className="attribution">
+			<a href="https://darksky.net/poweredby/">
+				<img src={img} height="38" width="95"/>
+			</a>
+		</div>
       </nav>
+
+
     );
   }
 }
